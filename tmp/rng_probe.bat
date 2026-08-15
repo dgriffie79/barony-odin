174b4e791ed7114ab5e3f0b80f10ccd66e3d9503
dgriffie79 <52303023+dgriffie79@users.noreply.github.com>
2026-08-15 15:19:04 -0400

    Remove temp probe files

--- tmp/rng_probe.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d C:\dev\barony-odin\tmp
-"C:\Program Files\Microsoft Visual Studio\18\Community\VC\Tools\Llvm\x64\bin\clang-cl.exe" /nologo /EHsc /Od /I. /Fe:C:\dev\barony-odin\tmp\rng_probe.exe C:\dev\barony-odin\tmp\rng_probe.cpp
-if errorlevel 1 exit /b 1
-C:\dev\barony-odin\tmp\rng_probe.exe
